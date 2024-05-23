67ebc32cffedbec66a2588e57a5044947891254d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-23 15:28:43 +0300

    Check that a cells first character is =

--- spreadsheetProject.js
+++ spreadsheetProject.js
@@ -45,5 +45,5 @@ window.onload = () => {
 const update = (event) => {
     const element = event.target;
     const value = element.value.replace(/\s/g, '');
-    if (!value.includes(element.id)) {}
+    if (!value.includes(element.id) && value[0] === '=') {}
 };
