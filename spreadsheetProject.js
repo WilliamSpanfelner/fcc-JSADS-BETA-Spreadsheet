ae1b01c698aa05732ead09cae555b709f57e1a1a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-23 12:37:10 +0300

    Create average func to return average value for array.

--- spreadsheetProject.js
+++ spreadsheetProject.js
@@ -1,7 +1,8 @@
 const isEven = (num) => num % 2 == 0;
 const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
+const average = (nums) => sum(nums) / nums.length;
 
-console.log(isEven(1), isEven(2)); // false true
+console.log(average([1, 2, 3])); // 2
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
 
