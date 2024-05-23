2c24a58a8e2dbcafe24771c5f5ed8b7cb9e55df5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-23 11:55:17 +0300

    Map the char codes to their corresponding chars

--- spreadsheetProject.js
+++ spreadsheetProject.js
@@ -1,7 +1,7 @@
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0));
-console.log(charRange("A", "J")); //[65, 66, 67, 68, 69, 70, 71, 72, 73, 74]
-// console.log(charRange(1, 10));
+const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
+console.log(charRange("A", "J")); //['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
 window.onload = () => {
     const container = document.getElementById("container");
     const createLabel = (name) => {
