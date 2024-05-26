2818f170f41f0e550596b3dc61cb9ae6ba27dd1c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-26 20:09:09 +0300

    Create func to split args by , and map the resullt using parseFloat

--- spreadsheetProject.js
+++ spreadsheetProject.js
@@ -36,6 +36,7 @@ const applyFunction = str => {
     const infix = /([\d.]+)([+-])([\d.]+)/;
     const str2 = infixEval(noHigh, infix);
     const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+    const toNumberList = (args) => args.split(',').map(parseFloat);
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
