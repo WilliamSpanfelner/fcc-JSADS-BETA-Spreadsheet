1cde577a9bfd04420ea4319d7a20d2e68064408c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-27 11:47:43 +0300

    Add someeven prop to return is some elements are even

--- spreadsheetProject.js
+++ spreadsheetProject.js
@@ -34,6 +34,7 @@ const spreadsheetFunctions = {
     lasttwo: (nums) => nums.slice(-2),
     has2: (nums) => nums.includes(2),
     increment: (nums) => nums.map((num) => num + 1),
+    someeven: (nums) => nums.some(isEven),
 };
 
 const applyFunction = str => {
